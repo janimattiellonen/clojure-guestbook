89599faa7f3c38f3639bf65596c8e46b10949541
Janimatti Ellonen <janimatti.ellonen@fraktio.fi>
2016-09-27 09:19:39 +0300

    moar tweaks

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,14 +1,3 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
-
--- :name update-user! :! :n
--- :doc update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
 
 -- :name get-user :? :1
 -- :doc retrieve a user given the id.
@@ -19,3 +8,12 @@ WHERE id = :id
 -- :doc delete a user given the id
 DELETE FROM users
 WHERE id = :id
+
+
+-- :name save-message! :! :n
+-- :doc creates a new message
+INSERT INTO guestbook (name, message) VALUES (:name, :message, :timestamp)
+
+-- :name get-messages :? :*
+-- :doc selects all available messages
+SELECT id, name, message, timestamp FROM guestbook
